93fbc6e71c7dc81519058e54e83021a9d89bb34c
ggsshh <90918666+ggsshh@users.noreply.github.com>
2023-06-14 22:13:55 +0800

    Update player.py

--- player.py
+++ player.py
@@ -1,23 +1,20 @@
 import pygame
 import sys
+import os
 from pygame.locals import *
 class Player(pygame.sprite.Sprite):    #继承Sprite精灵类
     def __init__(self):
         #设置血条
         self.blood = 3
         self.image = []   #用来存储玩家对象精灵图片的列表
-        for i in range(1,5):
-            self.image.append()
-        # self.rect = player_rect[0]  #初始化图片所在矩形
-        # self.rect.topleft = init_pos  #初始化矩形的左上角坐标
-        # self.speed = 8
-        # self.bullets = pygame.sprite.Group()
-        # self.ima_index = 0
-        # self.is_hit = False
-
-        # self.images.append(img)
-        # self.image = self.images[0]
-        self.rect = self.image.get_rect()
+        for i in range(1, 5):
+            img = pygame.image.load(os.path.join('images', 'player' + str(i) + '.png')).convert()
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+        self.x = 400
+        self.y = 300
+        #self.rect = self.image.get_rect()
         self.rect.x = 50
         self.rect.y = 50
         # self.image = None
@@ -26,20 +23,19 @@ class Player(pygame.sprite.Sprite):    #继承Sprite精灵类
         self.key_down_status = False
         self.key_up_status = False
     def update(self):
-        pass
-    def fire(self):
-        bullet = Bullet()
-        bullet.rect.bottom = self.rect.y + 10
-        self.bullets.add(bullet)
-    def move(self):
         if self.key_right_status:
-            self.rect.x += 5
+                self.x += 5
         if self.key_left_status:
-            self.rect.x -= 5
+                self.x -= 5
         if self.key_down_status:
-            self.rect.y += 5
+                self.y += 5
         if self.key_up_status:
-            self.rect.y -= 5
+                self.y -= 5
+    # def fire(self):
+    #     bullet = Bullet()
+    #     bullet.rect.bottom = self.rect.y + 10
+    #     self.bullets.add(bullet)
+
     def key_control(self):
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -65,3 +61,42 @@ class Player(pygame.sprite.Sprite):    #继承Sprite精灵类
                     self.key_up_status = False
                 elif event.key == K_s or event.key == K_DOWN:
                     self.key_down_status = False
+
+
+
+
+
+
+# def go_up_begin():
+#     Player.rect.y -= 10
+#
+#
+# def go_down_begin():
+#     Player.rect.y += 10
+#
+#
+# def go_left_begin():
+#     Player.rect.x -= 10
+#
+#
+# def go_right_begin():
+#     Player.rect.x += 10
+#
+#
+
+#
+# def go_up_end():
+#     return None
+#
+#
+# def go_down_end():
+#     return None
+#
+#
+# def go_left_end():
+#     return None
+#
+#
+# def go_right_end():
+#     return None
+#
